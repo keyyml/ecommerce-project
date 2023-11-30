d358acfb3290efb93296d276c19d5e02c192404e
keyla <keyyleiva@gmail.com>
2023-11-30 06:53:47 -0500

    added css

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,13 +1,15 @@
 import React from "react";
+import ProductCard from "./ProductCard";
 
 
-function ReviewCard({ user, comment, rating }) {
+function ReviewCard({ user, comment, rating, product }) {
  
 
   return (
-    <div className="hover:bg-gray-200 max-w-xs rounded overflow-hidden shadow-lg bg-white px-6 py-4">
-      <div className="font-bold text-xl mb-2">{user.first_name}</div>
-      <p className="text-gray-700 text-base">{comment}</p>
+    <div className="mt-4 hover:bg-gray-200 max-w-xs rounded overflow-hidden shadow-lg bg-white px-6 py-4">
+      <div className="font-bold text-xl mb-2">Review by: {user.first_name}</div>
+      <p className="text-gray-700 text-2xl font-bold">{comment}</p>
+      <p className="font-xsm">{product.name}</p>
       <div className="text-gray-700 font-bold text-xl flex justify-center">Rating: {rating}/10</div>
     </div>
 );
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,25 +1,43 @@
 import React from "react";
+import ReviewCard from "./ReviewCard";
 
 function User({ user }) {
 
-  console.log(user)
+  if (!user) {
+    return <h1>Loading...</h1>;
+  }
+
+  const renderReviews = user.reviews.map((review) => {
+    return (
+        <ReviewCard key={review.id} {...review} user = {user} />
+    )
+})
 
   return (
-    <>
-      {user ? (
-        <div>
-          <h1>Welcome to BeautyEcommerce</h1>
-          <h3>Account Info:</h3>
-          <p>Name: {user.first_name} {user.last_name}</p>
-          <p>Email: {user.email}</p>
-          <button>ADD ADDRESS</button>
-          <button>ADD PHONE NUMBER</button>
-          <h2>Reviews: </h2>
-        </div>
-      ) : (
-        <h1>LOADING..</h1>
-      )}
-    </>
+    <div className="bg-white p-6 shadow-md rounded-md">
+      <h1 className="text-4xl font-bold mb-4">Welcome to BeautyEcommerce</h1>
+      <div className="border-b border-gray-300 pb-4 mb-4">
+        <h3 className="text-2xl font-semibold mb-2">Account Info:</h3>
+        <p className="text-zinc-700">
+          <span className="mb-2 text-lg font-semibold">Name: {user.first_name} {user.last_name}</span>
+        </p>
+        <p className="text-zinc-700">
+          <span className="mb-2 text-lg font-semibold">Email: {user.email}</span>
+        </p>
+      </div>
+      <div className="flex space-x-4">
+        <button className="bg-violet-600 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          Add Address
+        </button>
+        <button className="bg-fuchsia-600 text-white px-4 py-2 rounded-md hover:bg-green-600">
+          Add Phone Number
+        </button>
+      </div>
+      <div className="mt-4">
+        <h2 className="text-lg font-semibold mb-2">Reviews:</h2>
+          {renderReviews}
+      </div>
+    </div>
   );
 }
 

88b388669201f115b8c860164b9d239bb279f6de
keyla <keyyleiva@gmail.com>
2023-11-30 02:10:44 -0500

    added tailwind css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,9 @@ import react from "react";
 
 function App() {
     return (
-        <h1> welcome </h1>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
 }
 

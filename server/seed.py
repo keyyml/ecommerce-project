d3cc03e0ffde123c3088b9a51ba8a874c2105f36
keyla <keyyleiva@gmail.com>
2023-11-30 05:06:44 -0500

    added prods

--- server/seed.py
+++ server/seed.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
             color = "Shades Row 1: Crater Brown, Beaver, English Walnut Row 2: Crown of Thorns, Di Serria, Roman Coffee",
             price = 12.99,
             brand = "Juvia's Place",
-            image = "URL"
+            image = "https://i.ibb.co/vHNpG77/juviaeye.png"
         ))
 
         products_to_add.append(Product(
@@ -69,44 +69,136 @@ if __name__ == '__main__':
             color = "Shade: 3.5 Natural Medium Brown",
             price = 26.00,
             brand = "Benefit Cosmetics",
-            image = "URL"
+            image = "https://i.ibb.co/vqYRv7D/goof.png"
         ))
 
         products_to_add.append(Product(
             id = 4,
             name = "Rosemary Mint Scalp & Hair Strengthening Oil",
-            color = "none",
+            color = "2.0 oz",
             price = 7.69,
             brand = "Mielle",
-            image = "URL"
+            image = "https://i.ibb.co/whg41H5/mielle.png"
         ))
 
         products_to_add.append(Product(
             id = 5,
             name = "No.7 Bonding Oil",
-            color = "none",
+            color = "1.0 oz",
             price = 30.00,
             brand = "OLAPLEX",
-            image = "URL"
+            image = "https://i.ibb.co/Dw4QvLp/ola.png"
         ))
 
         products_to_add.append(Product(
             id = 6,
             name = "Ceramide Retinol + HPR Rapid Skin Renewing Water Cream",
-            color = "none",
+            color = "1.7 oz",
             price = 92.00,
             brand = "Elizabeth Arden",
-            image = "URL"
+            image = "https://i.ibb.co/YBv5K75/earden.png"
         ))
 
         products_to_add.append(Product(
             id = 7,
             name = "Moisturizing Cream with Hyaluronic Acid for Balanced to Dry Skin",
-            color = "none",
+            color = "16.0 oz",
             price = 17.99,
             brand = "CeraVe",
-            image = "URL"
+            image = "https://i.ibb.co/LkKrxjn/cerave.png"
         ))
+
+        products_to_add.append(Product(
+            id = 8,
+            name = "All Nighter Waterproof Makeup Setting Spray",
+            color = "4.0 oz",
+            price = 36.00,
+            brand = "Urban Decay",
+            image = "https://i.ibb.co/FzxQd5g/ud.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 9,
+            name = "Pro Filt'r Soft Matte Longwear Liquid Foundation",
+            color = "Shade: 445 Size: 1.08 oz",
+            price = 40.00,
+            brand = "FENTY BEAUTY",
+            image = "https://i.ibb.co/M8zmL1P/fb1.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 10,
+            name = "Pro Filt'r Soft Matte Longwear Liquid Foundation",
+            color = "Color: Fairly Light 03 Size: 0.28 oz",
+            price = 38.00,
+            brand = "bareMinerals",
+            image = "https://i.ibb.co/RDgXhs1/foundation.png"
+        ))
+        
+        products_to_add.append(Product(
+            id = 11,
+            name = "Pro Filt'r Soft Matte Longwear Liquid Foundation",
+            color = "Shade: 420 Size: 1.08 oz",
+            price = 40.00,
+            brand = "FENTY BEAUTY",
+            image = "https://i.ibb.co/px4K9dQ/fb2.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 12,
+            name = "Pro Filt'r Soft Matte Longwear Liquid Foundation",
+            color = "Shade: 260 Size: 1.08 oz",
+            price = 40.00,
+            brand = "FENTY BEAUTY",
+            image = "https://i.ibb.co/CWFSmCd/fb3.png"
+        ))
+        
+        products_to_add.append(Product(
+            id = 13,
+            name = "Invite Only Mini Afterglow Liquid Blush Set",
+            color = "Size: 0.11 oz Shades: ORGASM (Peachy pink with golden shimmer) ORGASM RUSH (Rosy bronze) DOLCE VITA (Dusty rose)",
+            price = 31.50,
+            brand = "NARS",
+            image = "https://i.ibb.co/7Y3zsb3/blsset.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 14,
+            name = "Infallible 24H Fresh Wear Soft Matte Blush",
+            color = "Shade: 10 Confident Pink",
+            price = 15.99,
+            brand = "L'Oreal",
+            image = "https://i.ibb.co/gv7Rwgw/bls3.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 15,
+            name = "Glow Reviver Lip Oil",
+            color = "Color: Pink Quartz (light pink)",
+            price = 8.00,
+            brand = "e.l.f.",
+            image = "https://i.ibb.co/rxPRzSc/2615649.jpg"
+        ))
+
+        products_to_add.append(Product(
+            id = 16,
+            name = "Glow Reviver Lip Oil",
+            color = "Color: Honey Talks (brown-beige)",
+            price = 8.00,
+            brand = "e.l.f.",
+            image = "https://i.ibb.co/xDD3s29/26156502.png"
+        ))
+
+        products_to_add.append(Product(
+            id = 17,
+            name = "Maneater Mascara",
+            color = "Color: Black",
+            price = 25.00,
+            brand = "Tarte",
+            image = "https://i.ibb.co/FJbyjF3/memas.png"
+        ))
+
+
 
         db.session.add_all(products_to_add)
         db.session.commit()
@@ -302,6 +394,14 @@ if __name__ == '__main__':
             product_id = 7,
             category_id = 6
         ))
+
+        product_categories_to_add.append(ProductCategory(
+            id = 12,
+            product_id = 8,
+            category_id = 8
+        ))
+
+
 
         db.session.add_all(product_categories_to_add)
         db.session.commit()
